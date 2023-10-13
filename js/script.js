733186d802af2ba1bcdf839f1afb317e2c5c3db2
Bayaman <bkut.004@gmail.com>
2023-10-13 15:58:16 +0300

    Update script.js
    done first task. Work with objects

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
