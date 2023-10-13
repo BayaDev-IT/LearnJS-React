1b899eb3748ed1ac05ba08bee4993f3914a6abc8
Bayaman <bkut.004@gmail.com>
2023-10-13 20:37:54 +0300

    automated first task with cycles

--- js/script.js
+++ js/script.js
@@ -1,18 +1,51 @@
 "use strict"
 
-let num = 50; 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
-// while (num < 55) {
-//     console.log(num);
-//     num++;
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('Насколько оцените его?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+// let i = 0;
+// while (i< 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('Насколько оцените его?', '');
+
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++
 // }
+if(personalMovieDB.count < 10) {
+    console.log('Посмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла Ошибка')
+}
 
-// do {
-//     console.log(num);
-//     num++;
-// } while (num < 55)
 
-// for(let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++;
-// }
+console.log(personalMovieDB);
+
